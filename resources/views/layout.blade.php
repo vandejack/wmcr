32ce8c03b97240256d191264e85f8624c2e40ce1
yan043 <mahdian043@gmail.com>
2024-02-19 16:00:18 +0800

    fix: remove menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1809,104 +1809,10 @@
 												</div>
 												<div class="separator my-2"></div>
 												<div class="menu-item px-5">
-													<a href="../../demo2/dist/account/overview.html" class="menu-link px-5">My Profile</a>
+													<a href="/profile" class="menu-link px-5">My Profile</a>
 												</div>
 												<div class="menu-item px-5">
-													<a href="../../demo2/dist/pages/projects/list.html" class="menu-link px-5">
-														<span class="menu-text">My Projects</span>
-														<span class="menu-badge">
-															<span class="badge badge-light-danger badge-circle fw-bolder fs-7">3</span>
-														</span>
-													</a>
-												</div>
-												<div class="menu-item px-5" data-kt-menu-trigger="hover" data-kt-menu-placement="left-start">
-													<a href="#" class="menu-link px-5">
-														<span class="menu-title">My Subscription</span>
-														<span class="menu-arrow"></span>
-													</a>
-													<div class="menu-sub menu-sub-dropdown w-175px py-4">
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/referrals.html" class="menu-link px-5">Referrals</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/billing.html" class="menu-link px-5">Billing</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/statements.html" class="menu-link px-5">Payments</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/statements.html" class="menu-link d-flex flex-stack px-5">Statements
-															<i class="fas fa-exclamation-circle ms-2 fs-7" data-bs-toggle="tooltip" title="View your statements"></i></a>
-														</div>
-														<div class="separator my-2"></div>
-														<div class="menu-item px-3">
-															<div class="menu-content px-3">
-																<label class="form-check form-switch form-check-custom form-check-solid">
-																	<input class="form-check-input w-30px h-20px" type="checkbox" value="1" checked="checked" name="notifications" />
-																	<span class="form-check-label text-muted fs-7">Notifications</span>
-																</label>
-															</div>
-														</div>
-													</div>
-												</div>
-												<div class="menu-item px-5">
-													<a href="../../demo2/dist/account/statements.html" class="menu-link px-5">My Statements</a>
-												</div>
-												<div class="separator my-2"></div>
-												<div class="menu-item px-5" data-kt-menu-trigger="hover" data-kt-menu-placement="left-start">
-													<a href="#" class="menu-link px-5">
-														<span class="menu-title position-relative">Language
-														<span class="fs-8 rounded bg-light px-3 py-2 position-absolute translate-middle-y top-50 end-0">English
-														<img class="w-15px h-15px rounded-1 ms-2" src="assets/media/flags/united-states.svg" alt="" /></span></span>
-													</a>
-													<div class="menu-sub menu-sub-dropdown w-175px py-4">
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/settings.html" class="menu-link d-flex px-5 active">
-															<span class="symbol symbol-20px me-4">
-																<img class="rounded-1" src="assets/media/flags/united-states.svg" alt="" />
-															</span>English</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/settings.html" class="menu-link d-flex px-5">
-															<span class="symbol symbol-20px me-4">
-																<img class="rounded-1" src="assets/media/flags/spain.svg" alt="" />
-															</span>Spanish</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/settings.html" class="menu-link d-flex px-5">
-															<span class="symbol symbol-20px me-4">
-																<img class="rounded-1" src="assets/media/flags/germany.svg" alt="" />
-															</span>German</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/settings.html" class="menu-link d-flex px-5">
-															<span class="symbol symbol-20px me-4">
-																<img class="rounded-1" src="assets/media/flags/japan.svg" alt="" />
-															</span>Japanese</a>
-														</div>
-														<div class="menu-item px-3">
-															<a href="../../demo2/dist/account/settings.html" class="menu-link d-flex px-5">
-															<span class="symbol symbol-20px me-4">
-																<img class="rounded-1" src="assets/media/flags/france.svg" alt="" />
-															</span>French</a>
-														</div>
-													</div>
-												</div>
-												<div class="menu-item px-5 my-1">
-													<a href="../../demo2/dist/account/settings.html" class="menu-link px-5">Account Settings</a>
-												</div>
-												<div class="menu-item px-5">
-													<a href="../../demo2/dist/authentication/flows/basic/sign-in.html" class="menu-link px-5">Sign Out</a>
-												</div>
-												<div class="separator my-2"></div>
-												<div class="menu-item px-5">
-													<div class="menu-content px-5">
-														<label class="form-check form-switch form-check-custom form-check-solid pulse pulse-success" for="kt_user_menu_dark_mode_toggle">
-															<input class="form-check-input w-30px h-20px" type="checkbox" value="1" name="mode" id="kt_user_menu_dark_mode_toggle" data-kt-url="../../demo2/dist/index.html" />
-															<span class="pulse-ring ms-n1"></span>
-															<span class="form-check-label text-gray-600 fs-7">Dark Mode</span>
-														</label>
-													</div>
+													<a href="/logout" class="menu-link px-5">Sign Out</a>
 												</div>
 											</div>
 										</div>
