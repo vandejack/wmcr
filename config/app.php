9e2b861172f7882a8f1438125f184ad70df86db9
yan043 <mahdian043@gmail.com>
2024-02-20 09:16:43 +0800

    add: plugin captcha + excel

--- config/app.php
+++ config/app.php
@@ -168,6 +168,9 @@ return [
          * Package Service Providers...
          */
 
+        Mews\Captcha\CaptchaServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
@@ -225,7 +228,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
+        'Captcha' => Mews\Captcha\Facades\Captcha::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
