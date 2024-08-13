79c2ff87941fcf3fc42ce3dfbfe5ceb3c1ccf9d3
Ifan's ROG <d2n.money@gmail.com>
2024-08-13 17:44:31 +0800

    update grab

--- app/Http/Controllers/GrabController.php
+++ app/Http/Controllers/GrabController.php
@@ -23,6 +23,7 @@ class GrabController extends Controller
             self::StarclicktoBasket($r->sector_id,'TSEL',$id,$dateStart,$dateEnd);
             // starclick AO_TLKM
             self::StarclickOnetoBasket($r->sector_id,'TLKM',$id,$dateStart,$dateEnd);
+            self::api_insera_to_wmcr($r->sector_id,$dateStart,$dateEnd);
         }
         
     }
@@ -236,6 +237,89 @@ class GrabController extends Controller
         );
         $sql = "INSERT INTO {$table}({$columns}) VALUES {$values} ON DUPLICATE KEY UPDATE {$updates}";
         return \DB::statement($sql);
-    }    
+    }   
+    
+    public static function api_insera_to_wmcr($sector, $dateStart, $dateEnd)
+    {
+        DB::statement('
+                DELETE
+                    a1
+                FROM wmcr_source_insera a1
+                INNER JOIN wmcr_source_insera a2
+                WHERE
+                    a1.id > a2.id
+                    AND
+                    a1.incident_id = a2.incident_id
+            ');
+
+
+        $data = DB::table('wmcr_source_insera AS wsi')
+                    ->leftJoin('wmcr_master_witel AS wmw', 'wsi.witel', '=', 'wmw.name')
+                    ->leftJoin('wmcr_sector_alpro AS wsa', 'wsi.odp_name', '=', 'wsa.name')
+                    ->select(DB::raw('
+                        wmw.id AS witel_id,
+                        wsa.sector_id,
+                        wsi.incident_id,
+                        wsi.incident,
+                        (
+                            CASE
+                                WHEN (wsi.customer_segment IN ("DCS", "PL-TSEL") AND wsi.customer_type IN ("HVC_GOLD", "HVC_SILVER", "REGULER") AND wsi.owner_group LIKE "TA HD WITEL%") THEN "reguler_b2c"
+                                WHEN (wsi.customer_segment IN ("DES", "DBS", "DGS", "DPS", "DSS", "REG", "DWS", "TAW") AND wsi.owner_group LIKE "TA HD WITEL%") THEN "reguler_b2b"
+                                WHEN (wsi.customer_segment IN ("DCS", "PL-TSEL") AND wsi.customer_type IN ("HVC_VVIP", "HVC_DIAMOND", "HVC_PLATINUM") AND wsi.owner_group LIKE "TA HD WITEL%") THEN "hvc_b2c"
+                                WHEN (wsi.source_ticket IN ("PROACTIVE_TICKET", "PROACTIVE") AND wsi.reported_by = "PROACTIVE_TICKET") THEN "sqm"
+                                WHEN (wsi.source_ticket IN ("PROACTIVE_TICKET", "PROACTIVE") AND wsi.reported_by LIKE "PROMAN%") THEN "unspec"
+                                WHEN (wsi.guarante_status = "GUARANTEE") THEN "ffg"
+                                ELSE null
+                            END
+                        ) AS tipe_order
+                    '))
+                    ->where([
+                        ['wsi.incident', 'LIKE', 'INC%'],
+                        ['wsa.sector_id', '=', $sector]
+                    ])
+                    ->whereBetween('wsi.date_reported', [$dateStart, $dateEnd])
+                    ->whereNotNull('wsa.name')
+                    ->groupBy('wsi.incident_id')
+                    ->get();
+
+        $insert = [];
+
+        foreach ($data as $v)
+        {
+            if ($v->tipe_order !== null)
+            {
+                $tipe_order = DB::table('wmcr_order_type')
+                                ->where('aliases', $v->tipe_order)
+                                ->first();
+
+                if ($tipe_order)
+                {
+                    $insert[] = [
+                        'source_id'     => $v->incident_id,
+                        'witel_id'      => $v->witel_id,
+                        'sector_id'     => $v->sector_id,
+                        'order_type_id' => $tipe_order->id
+                    ];
+                }
+            }
+
+            // print_r("$v->incident tipe order $v->tipe_order \n");
+        }
+
+        if (!empty($insert)) {
+            self::insertOrUpdate($insert, 'wmcr_order_basket');
+        }
+
+        DB::statement('
+                DELETE
+                    a1
+                FROM wmcr_order_basket a1
+                INNER JOIN wmcr_order_basket a2
+                WHERE
+                    a1.id > a2.id
+                    AND
+                    a1.source_id = a2.source_id
+            ');
+    }
 
 }
